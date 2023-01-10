d0ea938e80bc4ed7f4a26465a13a409bc09aaef7
javkhalid <javkhalid@outlook.com>
2023-01-10 13:58:12 +0000

    unified model added.

---
+++ tartarus/app/azfunc/Services/Interface/IResponseService.cs
@@ -0,0 +1,10 @@
+﻿using tartarus.Model.Vulnerability;
+using Tartarus.Services;
+
+namespace tartarus.Services.Interface
+{
+    public interface IResponseService<T> where T : class
+    {
+        public ResponseData<T> GetResponseData(string URL, IVulnerabilityService _vulnerabilityService);
+    }
+}
